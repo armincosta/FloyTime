5d51127235ac9d0eadc0ae53965afc886f70b975
Costa Armin <armin.costa@eurac.edu>
2020-12-18 21:56:33 +0100

    Added game description

--- FloyTime.java
+++ FloyTime.java
@@ -1,9 +1,37 @@
-//Author: Armin Costa  ACosta@unibz.it
-//----------------------------
-// I coded this somewhat stupid game as an example for a freind of mine in order to lern programming
-//
-// So I hope it helps!! ;-)
+/** Author: Armin Costa  e-mail: armincosta@hotmail.com
+----------------------------
+ I coded this somewhat stupid game as an example for a freind of mine in order to lern programming
 
+The gams consists in:
+
+	* a 2D room with a window and a small container with some water
+
+	* a fly flying aroud
+
+	* try to get the fly into the container
+
+	* position the container near the window
+
+	* open the window ("S" key) and release the fly ("A" key)
+
+	* close the window as soon as possible ("D" key)
+
+	* when the window is open more flies might come in:)
+
+A mad Game!!!:D
+
+I coded it while being in a hostpital, and while at 1st year of university.
+
+ NOTE: Full of bug to be solved by some java beginner:)
+
+To run the java applet use the java appletviewer:
+```
+appletviewer FloyTime.html
+```
+ 
+ So I hope it helps!! ;-)
+
+**/
 import java.awt.*;
 import java.applet.*;
 import java.awt.event.*;
